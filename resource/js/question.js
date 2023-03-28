00d7d11c634ac280418db42419f90a7ccf15e748
Yamir Alim <yamiralim23@gmail.com>
2023-03-28 01:12:41 -0400

    final files

---
+++ resource/js/question.js
@@ -0,0 +1,27 @@
+var question = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<javascript>", "<scripting>", "<js>", "<script>"],
+        answer: "<script>"
+    },
+    {
+        title: "Where is the correct place to insert a JavaScript?.",
+        choices: ["The <body> section", "The <head> section", "Both the <head> section and the <body> section are correct"],
+        answer: "Both the <head> section and the <body> section are correct"
+    },
+    {
+        title: "What javascipt method can we use to select an html element?",
+        choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+        answer: "Both 1 and 3"
+    },
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function myFunction()", "function = myFunction()", "function:myFunction()"],
+        answer: "function myFunction()"
+    },
+    {
+        title: "The external JavaScript file must contain the <script> tag.",
+        choices: ["False", "True"],
+        answer: "False"
+    }
+]
